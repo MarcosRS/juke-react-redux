9d5b4e67483938d1abd01b3a3db72dae19a4250f
MarcosRS <MarcosRS16@gmail.com>
2017-04-03 18:22:24 -0400

    juke-react-redux-completed

--- browser/react/containers/AddSongContainer.js
+++ browser/react/containers/AddSongContainer.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import AddSong from '../components/AddSong';
-import store from '../store';
+// import store from '../store';
 import {loadAllSongs, addSongToPlaylist} from '../action-creators/playlists';
 
-class AddSongContainer extends React.Component {
+class AddSongContainerMethods extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = Object.assign({
+    this.state = {
       songId: 1,
       error: false
-    }, store.getState());
+    }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
 
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
+    // this.unsubscribe = store.subscribe(() => {
+    //   this.setState(store.getState());
+    // });
 
-    store.dispatch(loadAllSongs());
+    this.props.loadAllSongs();
 
   }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+  // componentWillUnmount() {
+  //   this.unsubscribe();
+  // }
 
   handleChange(evt) {
     this.setState({
@@ -37,20 +38,20 @@ class AddSongContainer extends React.Component {
   }
 
   handleSubmit(evt) {
-
+ 
     evt.preventDefault();
 
-    const playlistId = this.state.playlists.selected.id;
+    const playlistId = this.props.playlists.selected.id;
     const songId = this.state.songId;
 
-    store.dispatch(addSongToPlaylist(playlistId, songId))
-      .catch(() => this.setState({ error: true }));
+   this.props.addSongToPlaylist(playlistId, songId)
+   .catch(() => this.setState({ error: true }));
 
   }
 
   render() {
-
-    const songs = this.state.songs;
+     
+    const songs = this.props.songs;
     const error = this.state.error;
     const songId = this.state.songId;
 
@@ -65,5 +66,32 @@ class AddSongContainer extends React.Component {
     );
   }
 }
+
+//---------------------------------------------------------
+
+
+//Actual Connection :
+
+const mapStateToProps = function(state, ownProps){
+
+  return {
+    songs:state.songs,
+    playlists:state.playlists
+  }
+}
+
+const mapDispatchToProps = function(dispatch){
+  return {
+    loadAllSongs: function (){
+      dispatch(loadAllSongs())  
+    },
+    addSongToPlaylist:function(playlistId, songId){
+       return dispatch(addSongToPlaylist(playlistId, songId))
+    }
+  }
+}
+
+
+const AddSongContainer =connect(mapStateToProps, mapDispatchToProps)(AddSongContainerMethods) 
 
 export default AddSongContainer;
--- browser/react/containers/AlbumsContainer.js
+++ browser/react/containers/AlbumsContainer.js
@@ -1,28 +1,46 @@
 import React, {Component} from 'react';
-import store from '../store';
+import {connect} from 'react-redux'
+// import store from '../store';
 import Albums from '../components/Albums';
 
-class AlbumsContainer extends Component {
+// class AlbumsContainer extends Component {
 
-  constructor() {
-    super();
-    this.state = store.getState().albums;
+//   constructor() {
+//     super();
+//     this.state = store.getState().albums;
+//   }
+
+//   componentDidMount() {
+//     this.unsubscribe = store.subscribe(() => {
+//       this.setState(store.getState().albums);
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   render() {
+//     return <Albums albums={this.state.list}/>;
+//   }
+
+// }
+
+// export default AlbumsContainer;
+
+
+const mapStateToProps = function(state, ownProps){
+
+  return {
+    albums:state.albums.list
   }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().albums);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return <Albums albums={this.state.list}/>;
-  }
-
 }
 
-export default AlbumsContainer;
+const mapDispatchToProps = function(dispatch){
+  return {}
+}
+
+const AlbumsContainer = connect(mapStateToProps,mapDispatchToProps)(Albums)
+
+
+export default AlbumsContainer
